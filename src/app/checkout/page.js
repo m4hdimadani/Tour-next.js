791a32a1b72399418fa95b83faa93c3d8f8c32ff
MAHDIMADANI <m4hdim4d4ni@gmail.com>
2024-12-26 21:23:55 +0330

    create checkout

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,15 +1,36 @@
 "use client";
 
+import Footer from "@/components/organisms/Footer";
+import Header from "@/components/organisms/Header";
+import { useGetBasket } from "@/core/service/queries";
+import styles from "@/app/styles/checkout.module.css";
+
+import { FaUserAlt } from "react-icons/fa";
+
+
 function CheckOut() {
- 
-  
+  const { data, isPending } = useGetBasket();
+
+  if (!isPending) return <p>Loading ...</p>;
 
   return (
-    <div>
-      <div>
-        <h1>hi</h1>
+    <>
+      <Header />
+      <div className={styles.box}>
+        <div className={styles.rightBox}>
+          <div>
+            <FaUserAlt />
+            <h1>مشخصات مسافر</h1>
+          </div>
+          <div>
+            <input placeholder="نام و نام خانوادگی" />
+            <input placeholder=" کدملی " />
+          </div>
+        </div>
+        <div className={styles.leftBox}></div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
